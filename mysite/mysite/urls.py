7436ee4510ba24ad7927c9e61220fb60f5849c90
burningmemory <91364942+burningmemory@users.noreply.github.com>
2022-11-01 20:47:48 +0300

    Finished with users

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('books/', include('books.urls')),
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls'))
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/', include('users.urls'))
 ]
